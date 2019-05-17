2d8d49b97feb2f1af5007bb6fc54f29737b2d4c0
JadenJunLanLiu <2731468048@qq.com>
2019-05-17 20:34:57 +0800

    my first commit for eclipse

---
+++ src/test/java/com/atguigu/git/AppleTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
